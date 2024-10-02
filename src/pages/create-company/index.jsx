79c3d879e08a921cacdce5f625b56df3cc7d072e
Joao Almeida <joao.mnp.almeida@gmail.com>
2024-10-02 12:28:43 +0100

    Improvements to create-employments

--- src/pages/create-company/index.jsx
+++ src/pages/create-company/index.jsx
@@ -1,5 +1,5 @@
 // src/Company_Creation/App.js
-import { useEffect, useState, useCallback } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { getClientCredentialsToken } from "@/utils/auth-utils.js";
 import Form from "@/components/form/Form.jsx";
@@ -7,7 +7,7 @@ import DynamicForm from "@/components/form/DynamicForm.jsx";
 import { CredsForm } from "@/components/CredentialsForm.jsx";
 import DisplayResult from "@/utils/DisplayResult.jsx";
 import { FormArea, Error, ResultArea, HomeButton } from "@/App.styled.jsx";
-import * as Yup from "yup";
+import { object, string, boolean } from "yup";
 
 const credentials = {
   clientId: import.meta.env.REACT_APP_CLIENT_ID || "",
@@ -16,11 +16,85 @@ const credentials = {
   gatewayUrl: import.meta.env.REACT_APP_GATEWAY_URL || "",
 };
 
+const initialFormFields = [
+  {
+    name: "company_owner_email",
+    label: "Owner Email",
+    type: "text",
+    defaultValue: "",
+  },
+  {
+    name: "company_owner_name",
+    label: "Owner Name",
+    type: "text",
+    defaultValue: "",
+  },
+  {
+    name: "country_code",
+    label: "Country Code",
+    type: "text",
+    defaultValue: "GBR",
+  },
+  {
+    name: "desired_currency",
+    label: "Desired Currency",
+    type: "text",
+    defaultValue: "GBP",
+  },
+  {
+    name: "name",
+    label: "Company Name",
+    type: "text",
+    defaultValue: "",
+  },
+  {
+    name: "phone_number",
+    label: "Phone Number",
+    type: "text",
+    defaultValue: "",
+  },
+  {
+    name: "tax_number",
+    label: "Tax Number",
+    type: "text",
+    defaultValue: "",
+  },
+  {
+    name: "terms_of_service_accepted_at",
+    label: "Terms of Service Accepted At",
+    type: "text",
+    defaultValue: new Date().toISOString(),
+  },
+  {
+    name: "get_oauth_access_tokens",
+    label: "Get OAuth Access Tokens",
+    type: "checkbox",
+    defaultValue: false,
+  },
+  {
+    name: "send_create_password_email",
+    label: "Send Create Password Email",
+    type: "checkbox",
+    defaultValue: false,
+  },
+];
+
+const validationSchema = object({
+  company_owner_email: string().email("Invalid email").required("Required"),
+  company_owner_name: string().required("Required"),
+  country_code: string().required("Required"),
+  desired_currency: string().required("Required"),
+  name: string().required("Required"),
+  phone_number: string().required("Required"),
+  tax_number: string().required("Required"),
+  terms_of_service_accepted_at: string().required("Required"),
+  get_oauth_access_tokens: boolean(),
+  send_create_password_email: boolean(),
+});
+
 export function CompanyCreationPage() {
-  const [jsonSchema, setJsonSchema] = useState(null);
+  const [companyJsonSchema, setCompanyJsonSchema] = useState(null);
   const [accessToken, setAccessToken] = useState(null);
-  const [companyId, setCompanyId] = useState(null);
-  const [isAddressDetails, setIsAddressDetails] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [initialFormValues, setInitialFormValues] = useState(null);
   const [submissionStatus, setSubmissionStatus] = useState(null);
@@ -30,7 +104,7 @@ export function CompanyCreationPage() {
   const [creds, setCreds] = useState(credentials);
 
   // Fetch Access Token using the utility function
-  const fetchAccessToken = useCallback(async () => {
+  async function fetchAccessToken() {
     try {
       const token = await getClientCredentialsToken(
         creds,
@@ -39,51 +113,43 @@ export function CompanyCreationPage() {
       );
       setAccessToken(token);
       return token;
-    } catch (err) {
+    } catch {
       // Error is already handled within getClientCredentialsToken
       return null;
     }
-  }, [creds]);
+  }
 
-  const fetchSchema = useCallback(
-    async (endpoint) => {
-      setIsLoading(true);
-      setError(null);
-      const token = await fetchAccessToken();
+  async function fetchSchema(endpoint) {
+    setIsLoading(true);
+    setError(null);
+    const token = await fetchAccessToken();
 
-      if (token) {
-        try {
-          const response = await axios.get(endpoint, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-          setJsonSchema(response.data.data);
-        } catch (error) {
-          console.error("Error fetching JSON schema:", error);
-          setError(
-            `Error fetching form data: ${
-              error.response?.data?.message || error.message
-            }`
-          );
-        } finally {
-          setIsLoading(false);
-        }
+    if (token) {
+      try {
+        const response = await axios.get(endpoint, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setCompanyJsonSchema(response.data.data);
+      } catch (error) {
+        console.error("Error fetching JSON schema:", error);
+        setError(
+          `Error fetching form data: ${
+            error.response?.data?.message || error.message
+          }`
+        );
+      } finally {
+        setIsLoading(false);
       }
-    },
-    [fetchAccessToken]
-  );
-
-  useEffect(() => {
-    if (initialFormValues && !isAddressDetails) {
-      fetchSchema(
-        `/api/v1/companies/schema?country_code=${initialFormValues.country_code}&form=address_details`
-      );
     }
-  }, [fetchSchema, isAddressDetails, initialFormValues]);
+  }
 
   const handleInitialFormSubmit = (values) => {
     setInitialFormValues(values);
+    fetchSchema(
+      `/api/v1/companies/schema?country_code=${values.country_code}&form=address_details`
+    );
   };
 
   const handleSubmit = async (jsonValues) => {
@@ -91,6 +157,8 @@ export function CompanyCreationPage() {
       setError(null);
 
       // Determine the actions based on the selected checkboxes
+      const searchParams = new URLSearchParams();
+
       const actions = [];
       if (initialFormValues.get_oauth_access_tokens) {
         actions.push("get_oauth_access_tokens");
@@ -99,8 +167,9 @@ export function CompanyCreationPage() {
         actions.push("send_create_password_email");
       }
 
+      searchParams.append("actions", actions);
       const actionParam =
-        actions.length > 0 ? `?actions=${actions.join("%2C")}` : "";
+        actions.length > 0 ? `?${searchParams.toString()}` : "";
 
       const payload = {
         ...initialFormValues,
@@ -135,8 +204,6 @@ export function CompanyCreationPage() {
 
   const handleStartOver = () => {
     setInitialFormValues(null);
-    setCompanyId(null);
-    setIsAddressDetails(false);
     setSubmissionStatus(null);
     setResponseData(null); // Reset response data
     setError(null);
@@ -146,90 +213,12 @@ export function CompanyCreationPage() {
     setCreds(values);
   };
 
-  const initialFormFields = [
-    {
-      name: "company_owner_email",
-      label: "Owner Email",
-      type: "text",
-      defaultValue: "",
-    },
-    {
-      name: "company_owner_name",
-      label: "Owner Name",
-      type: "text",
-      defaultValue: "",
-    },
-    {
-      name: "country_code",
-      label: "Country Code",
-      type: "text",
-      defaultValue: "GBR",
-    },
-    {
-      name: "desired_currency",
-      label: "Desired Currency",
-      type: "text",
-      defaultValue: "GBP",
-    },
-    {
-      name: "name",
-      label: "Company Name",
-      type: "text",
-      defaultValue: "",
-    },
-    {
-      name: "phone_number",
-      label: "Phone Number",
-      type: "text",
-      defaultValue: "",
-    },
-    {
-      name: "tax_number",
-      label: "Tax Number",
-      type: "text",
-      defaultValue: "",
-    },
-    {
-      name: "terms_of_service_accepted_at",
-      label: "Terms of Service Accepted At",
-      type: "text",
-      defaultValue: new Date().toISOString(),
-    },
-    {
-      name: "get_oauth_access_tokens",
-      label: "Get OAuth Access Tokens",
-      type: "checkbox",
-      defaultValue: false,
-    },
-    {
-      name: "send_create_password_email",
-      label: "Send Create Password Email",
-      type: "checkbox",
-      defaultValue: false,
-    },
-  ];
-
-  const validationSchema = Yup.object({
-    company_owner_email: Yup.string()
-      .email("Invalid email")
-      .required("Required"),
-    company_owner_name: Yup.string().required("Required"),
-    country_code: Yup.string().required("Required"),
-    desired_currency: Yup.string().required("Required"),
-    name: Yup.string().required("Required"),
-    phone_number: Yup.string().required("Required"),
-    tax_number: Yup.string().required("Required"),
-    terms_of_service_accepted_at: Yup.string().required("Required"),
-    get_oauth_access_tokens: Yup.boolean(),
-    send_create_password_email: Yup.boolean(),
-  });
-
   return (
     <>
       <div className="App">
         <HomeButton to="/">Home</HomeButton>
         <CredsForm initialValues={creds} onSubmit={handleCredsSubmit} />
-        {!initialFormValues ? (
+        {!companyJsonSchema ? (
           <DynamicForm
             fields={initialFormFields}
             validationSchema={validationSchema}
@@ -250,8 +239,11 @@ export function CompanyCreationPage() {
             ) : (
               <FormArea>
                 <h1>Company Information Form</h1>
-                {jsonSchema && (
-                  <Form jsonSchema={jsonSchema} onSubmit={handleSubmit} />
+                {companyJsonSchema && (
+                  <Form
+                    jsonSchema={companyJsonSchema}
+                    onSubmit={handleSubmit}
+                  />
                 )}
                 {error && <Error>{error}</Error>}
               </FormArea>
