1ea8dcdb4994c70404113845d3bfd23615ddc40e
Joao Almeida <joao.mnp.almeida@gmail.com>
2024-10-09 14:04:08 +0100

    remove comment

--- src/lib/api.js
+++ src/lib/api.js
@@ -20,38 +20,6 @@ partnerApiClient.interceptors.request.use(
   }
 );
 
-// partnerApiClient.interceptors.response.use(
-//   (response) => response, // Directly return successful responses.
-//   async (error) => {
-//     const originalRequest = error.config;
-//     if (error.response.status === 401 && !originalRequest._retry) {
-//       originalRequest._retry = true; // Mark the request as retried to avoid infinite loops.
-//       try {
-//         const refreshToken = localStorage.getItem("refreshToken"); // Retrieve the stored refresh token.
-//         // Make a request to your auth server to refresh the token.
-//         const response = await getAccessToken({});
-//         const { accessToken, refreshToken: newRefreshToken } = response.data;
-//         // Store the new access and refresh tokens.
-//         localStorage.setItem("accessToken", accessToken);
-//         localStorage.setItem("refreshToken", newRefreshToken);
-//         // Update the authorization header with the new access token.
-//         partnerApiClient.defaults.headers.common[
-//           "Authorization"
-//         ] = `Bearer ${accessToken}`;
-//         return partnerApiClient(originalRequest); // Retry the original request with the new access token.
-//       } catch (refreshError) {
-//         // Handle refresh token errors by clearing stored tokens and redirecting to the login page.
-//         console.error("Token refresh failed:", refreshError);
-//         localStorage.removeItem("accessToken");
-//         localStorage.removeItem("refreshToken");
-//         window.location.href = "/login";
-//         return Promise.reject(refreshError);
-//       }
-//     }
-//     return Promise.reject(error); // For all other errors, return the error as is.
-//   }
-// );
-
 export const customerApiClient = axios.create({
   headers: {
     "Content-Type": "application/json",
